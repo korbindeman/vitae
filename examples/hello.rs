ec3f3d800841342172d4db6134ecabca372d9571
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2025-08-28 02:42:52 +0200

    Render text from draw commands

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,7 +1,10 @@
 use vitae::prelude::*;
 
 pub fn main() {
-    let root = div().size(FULL).bg(RED).child(div().size(px(400.)));
+    let root = div()
+        .size(FULL)
+        .bg(RED)
+        .child(div().size(px(400.)).child(text("Hello from Vitae")));
 
     let app = App::new(root);
 
